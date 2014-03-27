ed3b6e93d02a720a48f9eaa2f57636b0833b8003
robin <robin@mytechtip>
2014-03-27 12:08:13 +1100

    only allow one time picker to open when multiple exist on the same page

--- src/timepickerpop.js
+++ src/timepickerpop.js
@@ -2,71 +2,83 @@
  * Anularjs Module for pop up timepicker
  */
 angular.module('timepickerPop', [ 'ui.bootstrap' ])
+.factory('timepickerState', function() {
+  var pickers = [];
+  return {
+		addPicker: function(picker) {
+			pickers.push(picker);
+		},
+		closeAll: function() {
+			for (var i=0; i<pickers.length; i++) {
+				pickers[i].close();
+			}
+		}
+	};
+})
 .directive("timeFormat", function($filter) {
-      return {
-        restrict : 'A',
-        require : 'ngModel',
-        scope : {
-          showMeridian : '=',
-        },
-        link : function(scope, element, attrs, ngModel) {
-            var parseTime = function(viewValue) {
+  return {
+    restrict : 'A',
+    require : 'ngModel',
+    scope : {
+      showMeridian : '=',
+    },
+    link : function(scope, element, attrs, ngModel) {
+        var parseTime = function(viewValue) {
 
-            if (!viewValue) {
-              ngModel.$setValidity('time', true);
-              return null;
-            } else if (angular.isDate(viewValue) && !isNaN(viewValue)) {
-              ngModel.$setValidity('time', true);
-              return viewValue;
-            } else if (angular.isString(viewValue)) {
-              var timeRegex = /^(0?[0-9]|1[0-2]):[0-5][0-9] ?[a|p]m$/i;
-              if (!scope.showMeridian) {
-                timeRegex = /^([01]?[0-9]|2[0-3]):[0-5][0-9]$/;
+        if (!viewValue) {
+          ngModel.$setValidity('time', true);
+          return null;
+        } else if (angular.isDate(viewValue) && !isNaN(viewValue)) {
+          ngModel.$setValidity('time', true);
+          return viewValue;
+        } else if (angular.isString(viewValue)) {
+          var timeRegex = /^(0?[0-9]|1[0-2]):[0-5][0-9] ?[a|p]m$/i;
+          if (!scope.showMeridian) {
+            timeRegex = /^([01]?[0-9]|2[0-3]):[0-5][0-9]$/;
+          }
+          if (!timeRegex.test(viewValue)) {
+            ngModel.$setValidity('time', false);
+            return undefined;
+          } else {
+            ngModel.$setValidity('time', true);
+            var date = new Date();
+            var sp = viewValue.split(":");
+            var apm = sp[1].match(/[a|p]m/i);
+            if (apm) {
+              sp[1] = sp[1].replace(/[a|p]m/i, '');
+              if (apm[0].toLowerCase() == 'pm') {
+                sp[0] = sp[0] + 12;
               }
-              if (!timeRegex.test(viewValue)) {
-                ngModel.$setValidity('time', false);
-                return undefined;
-              } else {
-                ngModel.$setValidity('time', true);
-                var date = new Date();
-                var sp = viewValue.split(":");
-                var apm = sp[1].match(/[a|p]m/i);
-                if (apm) {
-                  sp[1] = sp[1].replace(/[a|p]m/i, '');
-                  if (apm[0].toLowerCase() == 'pm') {
-                    sp[0] = sp[0] + 12;
-                  }
-                }
-                date.setHours(sp[0], sp[1]);
-                return date;
-              };
-            } else {
-              ngModel.$setValidity('time', false);
-              return undefined;
-            };
-          };
-
-          ngModel.$parsers.push(parseTime);
-
-          var showTime = function(data) {
-            parseTime(data);
-            var timeFormat = (!scope.showMeridian) ? "HH:mm" : "hh:mm a";
-            return $filter('date')(data, timeFormat);
-          };
-          ngModel.$formatters.push(showTime);
-          scope.$watch('showMeridian', function(value) {
-            var myTime = ngModel.$modelValue;
-            if (myTime) {
-              element.val(showTime(myTime));
             }
-
-          });
-
-        }
+            date.setHours(sp[0], sp[1]);
+            return date;
+          };
+        } else {
+          ngModel.$setValidity('time', false);
+          return undefined;
+        };
       };
+
+      ngModel.$parsers.push(parseTime);
+
+      var showTime = function(data) {
+        parseTime(data);
+        var timeFormat = (!scope.showMeridian) ? "HH:mm" : "hh:mm a";
+        return $filter('date')(data, timeFormat);
+      };
+      ngModel.$formatters.push(showTime);
+      scope.$watch('showMeridian', function(value) {
+        var myTime = ngModel.$modelValue;
+        if (myTime) {
+          element.val(showTime(myTime));
+        }
+
+      });
+    }
+  };
 })
 
-.directive('timepickerPop', function($document) {
+.directive('timepickerPop', function($document, timepickerState) {
       return {
         restrict : 'E',
         transclude : false,
@@ -78,14 +90,29 @@ angular.module('timepickerPop', [ 'ui.bootstrap' ])
           $scope.isOpen = false;
 
           $scope.toggle = function() {
-            $scope.isOpen = !$scope.isOpen;
-          };
-
-          $scope.open = function() {
-            $scope.isOpen = true;
+        	if ($scope.isOpen) {
+        		$scope.close();
+        	} else {
+        		$scope.open();
+        	}
           };
         },
         link : function(scope, element, attrs) {
+          var picker = {
+        		  open : function () {
+        			  timepickerState.closeAll();
+        			  scope.isOpen = true;
+        		  },
+        		  close: function () {
+        			  scope.isOpen = false;
+        		  }
+          		  
+          }
+          timepickerState.addPicker(picker);
+          
+          scope.open = picker.open;
+          scope.close = picker.close;
+          
           scope.$watch("inputTime", function(value) {
             if (!scope.inputTime) {
               element.addClass('has-error');
@@ -102,7 +129,7 @@ angular.module('timepickerPop', [ 'ui.bootstrap' ])
 
           $document.bind('click', function(event) {
             scope.$apply(function() {
-              scope.isOpen = false;
+           		scope.isOpen = false;
             });
           });
 
